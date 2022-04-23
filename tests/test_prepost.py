6d03550dc83adc194f68c59a7712ba4f6a3c7de4
pub12 <pubudu79@gmail.com>
2022-04-23 14:34:42 +0000

    Added match_pattern decorator for REs

--- tests/test_prepost.py
+++ tests/test_prepost.py
@@ -9,28 +9,37 @@ from prepost import PreCond, PostCond
 class TestPre(unittest.TestCase):
 
 	# print('sssss')
-	def test_equals_precondition_fail(self):              
-		@PreCond.equals( {'fruit':'xx'} )
+	def test_0010_equals_precondition_fail(self):              
+		@PreCond.equals( fruit='xx' )
 		def func_test(*, fruit, size):
 			print(f"fruit={fruit} and size={size}")
 
 		self.assertRaises(Exception, func_test, fruit='apple', size='1kg' )
 
-	def test_equals_precondition_success(self):              
-		@PreCond.equals( {'fruit':'apple'} )
+	def test_0020_equals_precondition_success(self):              
+		@PreCond.equals( fruit= 'apple' )
 		def func_test(*, fruit, size):
 			print(f"fruit={fruit} and size={size}")
 
 		func_test( fruit='apple', size='1kg')
 
-	def test_not_null_precondition_success(self):              
+
+	def test_0030_equals_precondition_success_2(self):              
+		@PreCond.equals( fruit= 'apple', size='1kg' )
+		def func_test(*, fruit, size):
+			print(f"fruit={fruit} and size={size}")
+
+		func_test( fruit='apple', size='1kg')
+
+
+	def test_0040_not_null_precondition_success(self):              
 		@PreCond.not_null( ['fruit'] )
 		def func_test(*, fruit, size):
 			print(f"fruit={fruit} and size={size}")
 
 		func_test( fruit='apple', size='1kg')
 
-	def test_not_null_precondition_fail(self):              
+	def test_0041_not_null_precondition_fail(self):              
 		@PreCond.not_null( ['fruit', 'size'] )
 		def func_test(*, fruit, size):
 			print(f"fruit={fruit} and size={size}")
@@ -38,16 +47,53 @@ class TestPre(unittest.TestCase):
 		self.assertRaises(Exception, func_test, fruit='apple' )
 
 
-	def test_pre_dict_fields(self):              
+	def test_0050_pre_dict_fields(self):              
 		@PreCond.dict_has_fields( fruit_dict=['apple', 'banana']  )
 		def func_test(*, fruit_dict):
 			print(f"{fruit_dict}")
 
-		func_test( fruit_dict={'apple':'1kg' } )
+		self.assertRaises(Exception, func_test, fruit_dict={'apple':'1kg'} )
+
+	def test_0060_minimum_not_null_fail(self):
+		@PreCond.minimum_not_null( 1 )
+		def func_test(*, fruit=None, avacado=None):
+			print(f"{fruit}")
+
+		self.assertRaises(Exception, func_test)
+
+	def test_0070_minimum_not_success_1(self):
+		@PreCond.minimum_not_null( 1 )
+		def func_test(*, fruit=None, avacado=None):
+			print(f"{fruit}")
+
+		func_test( fruit="apple")
+
+	def test_0080_minimum_not_success_2(self):
+		@PreCond.minimum_not_null( 1 )
+		def func_test(*, fruit=None, avacado=None):
+			print(f"{fruit}")
+
+		func_test( avacado="large")
+
+	def test_0090_minimum_not_success_3(self):
+		@PreCond.minimum_not_null( 1 )
+		def func_test(*, fruit=None, avacado=None):
+			print(f"{fruit}")
+
+		func_test( fruit="apple", avacado="large")
+
+	def test_0091_maches_pattern(self):
+		@PreCond.matches_pattern( fruit='^[a-zA-Z]+$')
+		def func_test(*, fruit=None, avacado=None):
+			print(f"{fruit}")
+
+		func_test( fruit="apple")
+		self.assertRaises(Exception, func_test, fruit="apple1")
+		self.assertRaises(Exception, func_test, "apple1")
 
 
 class TestPost(unittest.TestCase):
-	def test_post_not_null(self):              
+	def test_0100_post_not_null(self):              
 		@PostCond.not_null()
 		def func_test(*, fruit, size):
 			print(f"fruit={fruit} and size={size}")
